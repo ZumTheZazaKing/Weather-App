877a4388513ddd55ee88b8a120442130cd13542d
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-19 16:52:52 +0800

    Design added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import humidityImage from './images/humidity.png';
+import windSpeedImage from './images/wind.png';
+import compass from './images/compass.png';
+import binoculars from './images/binoculars.png';
+import sunriseImage from './images/sunrise.png';
+import sunsetImage from './images/sunset.png';
 
 function App() {
 
@@ -7,6 +13,7 @@ function App() {
   let [cityName, setCityName] = useState("");
   let [country,setCountry] = useState("");
   let [description, setDescription] = useState("");
+  let [weatherIcon, setWeatherIcon] = useState("");
   let [temp, setTemp] = useState("");
   let [feelTemp, setFeelTemp] = useState("");
 
@@ -25,7 +32,8 @@ function App() {
     .then(data => {
       setCityName(data.name);
       setCountry(data.sys.country);
-      setDescription(data.weather.description);
+      setDescription(data.weather[0].description);
+      setWeatherIcon("http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
       setTemp(data.main.temp);
       setFeelTemp(data.main.feels_like);
 
@@ -58,23 +66,27 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{cityName},{country}</h1>
-      <h2>{description}</h2>
-      <h3>{(temp - 273.15).toFixed(0)}{"°C"}</h3>
-      <h4>Feels like {(feelTemp - 273.15).toFixed(0)}{"°C"}</h4>
-      <br/><br/>
-      <form onSubmit={search}>
-        <input type="text" placeholder="Enter city name" value={inputCity} onChange={e => setInputCity(e.target.value)} required/>
-        <input type="submit" value="Search"/>
-      </form>
-      <p>
-        Humidity: {humidity}%<br/><br/>
-        Wind Speed: {windSpeed} m/s<br/><br/>
-        Wind Direction: {windDirection}°<br/><br/>
-        Visibility: {visibility} km<br/><br/>
-        Sunrise: {sunrise}<br/><br/>
-        Sunset: {sunset}<br/><br/>
-      </p>
+      <div id="LeftSide">
+        <h1>{cityName}, {country}</h1>
+        <h2>{description}</h2>
+        <img src={weatherIcon} alt="" width="100" height="100"/>
+        <p id="temp">{(temp - 273.15).toFixed(0)}{"°C"}</p>
+        <h4>Feels like {(feelTemp - 273.15).toFixed(0)}{"°C"}</h4>
+      </div>
+      <div id="RightSide">
+        <form onSubmit={search}>
+          <input type="text" placeholder="Enter city name" value={inputCity} onChange={e => setInputCity(e.target.value)} required/>
+        </form>
+        <br/>
+        <div id="info">
+          <div>Humidity: <br/>{humidity}%<img src={humidityImage} alt="" width="50" height="50"/></div>
+          <div>Wind Speed: <br/>{windSpeed} m/s<img src={windSpeedImage} alt="" width="50" height="50"/></div>
+          <div>Wind Direction: <br/>{windDirection}°<img src={compass} alt="" width="50" height="50"/></div>
+          <div>Visibility: <br/>{visibility} km<img src={binoculars} alt="" width="50" height="50"/></div>
+          <div>Sunrise: <br/>{sunrise}<img src={sunriseImage} alt="" width="50" height="50"/></div>
+          <div>Sunset: <br/>{sunset}<img src={sunsetImage} alt="" width="50" height="50"/></div>
+        </div>
+      </div>
     </div>
   );
 }
